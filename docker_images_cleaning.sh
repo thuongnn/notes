c0be9187797b7e486a24d0a6e953eaf961823cdf
Nguyen Nhu Thuong <33452221+thuongnn@users.noreply.github.com>
2023-09-11 15:37:49 +0700

    Update docker_images_cleaning.sh

--- docker_images_cleaning.sh
+++ docker_images_cleaning.sh
@@ -10,18 +10,25 @@ days_threshold=3
 cutoff_date=$(date -u -d "@$((current_date - 86400 * days_threshold))" +%Y-%m-%dT%H:%M:%S.%N%z)
 
 # Get a list of unused images and their creation times
-unused_images=$(docker images --format "{{.ID}} {{.Created}}" --filter "dangling=true" | awk -v cutoff="$cutoff_date" '$2 < cutoff { print $1 }')
+unused_images=$(docker images -q --filter "dangling=true")
+# 86400 seconds = 1 day
 
 # Iterate through the list of unused images
 for image_id in $unused_images; do
-    # Check if this image is being used in a container or service
-    is_used=$(docker ps -a --format "{{.Image}}" | grep -c "$image_id")
-    
-    if [ $is_used -eq 0 ]; then
-        # Remove the image if it's not in use
-        echo "Removing image $image_id"
-        docker rmi $image_id
-    else
-        echo "Not removing image $image_id because it is in use"
+    # Get the creation time of the image
+    image_created=$(docker inspect -f '{{.Created}}' "$image_id")
+
+    # Check if the image creation time is earlier than the cutoff date
+    if [[ "$image_created" < "$cutoff_date" ]]; then
+        # Check if this image is being used in a container or service
+        is_used=$(docker ps -a --format "{{.Image}}" | grep -c "$image_id")
+
+        if [ $is_used -eq 0 ]; then
+            # Remove the image if it's not in use
+            echo "Removing image $image_id"
+            docker rmi $image_id
+        else
+            echo "Not removing image $image_id because it is in use"
+        fi
     fi
 done
